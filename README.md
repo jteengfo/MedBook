3aeedd35b00c7814893e0031e155b7da323f5e01
James Te Eng Fo <jamesang@ualberta.ca>
2021-12-22 17:02:31 -0700

    added project folders

---
+++ README.md
@@ -0,0 +1,7 @@
+# medBook
+it's a simple app that lets users add meds for tracking purposes 
+
+## Storyboard Sequence
+[FIGMA_LINK] (https://www.figma.com/file/CX26Le1djtNIT1j7jZcEBn/MedBook)
+
+![Storyboard Sequence image] (MedBook storyboard sequence.PNG)
---
+++ app/src/main/java/com/example/medbook/Medicine.java
@@ -0,0 +1,71 @@
+package com.example.medbook;
+
+import java.util.Date;
+
+public class Medicine {
+
+    // Initialize medicine attributes
+    private String name;
+    private int doseAmt;
+    private String doseUnit;
+    private int dailyFreq;
+    private Date date;
+
+    /**
+     * This is the medicine constructor
+     * @param name
+     * @param doseAmt
+     * @param doseUnit
+     * @param dailyFreq
+     * @param date
+     */
+    public Medicine(String name, int doseAmt, String doseUnit, int dailyFreq, Date date) {
+        this.name = name;
+        this.doseAmt = doseAmt;
+        this.doseUnit = doseUnit;
+        this.dailyFreq = dailyFreq;
+        this.date = date;
+    }
+
+    // setters and getters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDoseAmt() {
+        return doseAmt;
+    }
+
+    public void setDoseAmt(int doseAmt) {
+        this.doseAmt = doseAmt;
+    }
+
+    public String getDoseUnit() {
+        return doseUnit;
+    }
+
+    public void setDoseUnit(String doseUnit) {
+        this.doseUnit = doseUnit;
+    }
+
+    public int getDailyFreq() {
+        return dailyFreq;
+    }
+
+    public void setDailyFreq(int dailyFreq) {
+        this.dailyFreq = dailyFreq;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
